aa2103e27333c7ea176ecf66a3b1c24349753f48
Flavio Matheus Muniz Ribeiro da Silva <flaviommrs@gmail.com>
2016-11-25 16:18:13 -0200

    place holders para a tela de medicamentos e para a tela de previsão feitos. Na teoria o as informações ja estão sendo passadas

--- PrevisaodeEstoque/estoque/views.py
+++ PrevisaodeEstoque/estoque/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from models import Medicamento, Doenca, Causa, Trata, Relaciona
+
 # Create your views here.
 
 def updateDB(request):
@@ -8,3 +10,26 @@ def updateDB(request):
 
 def fit(request):
 	return HttpResponse("Should fit the machine learning")
+
+def medicamentos(request, nomeDoenca):
+	doenca = Doenca.objects.get(nome=nomeDoenca)
+	causas = Causa.objects.filter(doenca=doenca)
+	medicamentos = []
+
+	for causa in causas:
+		med = Trata.objects.filter(sintoma=causa.sintoma)
+		for m in med:
+			medicamentos.append(m)
+
+	context = {'medicamentos': medicamentos}
+	return render(request, 'estoque/medicamentos.html', context)
+
+# def previsao(request, idRemedio):
+# 	medicamento = Medicamentos.objects.get(registro=idRemedio)
+# 	trata = Trata.objects.get(medicamento=medicamento)
+# 	relaciona = Relaciona.objects(sintoma=trata.sintoma)
+
+
+# 	return HttpResponse("Should Return prevision")
+
+
