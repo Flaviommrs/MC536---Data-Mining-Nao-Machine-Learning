b5d43a08329009a3db9ff0f215584a7c7f80e599
Flavio Matheus Muniz Ribeiro da Silva <flaviommrs@gmail.com>
2016-11-25 19:17:49 -0200

    mudando front end para dentro de templates

--- PrevisaodeEstoque/estoque/urls.py
+++ PrevisaodeEstoque/estoque/urls.py
@@ -3,6 +3,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^updateDB/$', views.updateDB, name='updateDB'),
-    url(r'^fit/$', views.fit, name='fit'),
+    # url(r'^updateDB/$', views.updateDB, name='updateDB'),
+    # url(r'^fit/$', views.fit, name='fit'),
+    url(r'^filtro/$', views.filtro, name='filtro'),
+    url(r'^(?P<nomeDoenca>[A-Z][a-z]+)/$', views.medicamentos, name='medicamentos'),
 ]
